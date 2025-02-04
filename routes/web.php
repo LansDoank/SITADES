79a8f17c14239d9981a4c50776a097d9ce340da1
LansDoank <maulana09180@gmail.com>
2025-02-04 12:14:21 +0700

    sasa

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AdminController::class,'login'])->name('login.admin');
 
 Route::get('/login/admin',[AdminController::class,'login'])->name('login.admin');
-=======
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> c46bcdc51dc4087b060f2b4066ecfac92e3972dd
+
