bcebd28d8c345d334869440d99e9996176a2d003
LansDoank <maulana09180@gmail.com>
2025-02-04 11:48:52 +0700

    menambahkan login form

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [AdminController::class,'login'])->name('login.admin');
+
+Route::get('/login/admin',[AdminController::class,'login'])->name('login.admin');
